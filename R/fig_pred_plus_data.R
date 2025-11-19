bb96ccb20f72a900a3834f4903b3c179a6810a63
Kaitlyn Johnson <kaitlyn.johnson@lshtm.ac.uk>
2025-11-19 12:15:45 +0000

    add all plot components

--- R/fig_pred_plus_data.R
+++ R/fig_pred_plus_data.R
@@ -65,7 +65,50 @@ get_plot_model_pred_obs <- function(model_pred_obs_df,
     ylab("Model predicted\nobserved frequencies") +
     guides(
       fill = "none",
-      color = guide_legend(
+      color = "none"
+    ) +
+    scale_x_date(
+      limits = c(min(eval_seq$date), max(eval_seq$date)),
+      date_breaks = "1 week",
+      date_labels = "%d %b %Y"
+    ) +
+    geom_vline(aes(xintercept = ymd(nowcast_date)),
+      linetype = "dashed"
+    )
+  return(p)
+}
+
+get_plot_seq_count <- function(eval_seq,
+                               temporal_granularity) {
+  if (temporal_granularity == "weeks") {
+    eval_seq <- daily_to_weekly(eval_seq)
+  }
+
+  total_seq <- eval_seq |>
+    group_by(date, location) |>
+    summarise(n_seq = sum(sequences))
+  obs <- eval_seq |>
+    left_join(total_seq)
+  plot_comps <- plot_components()
+  p <- ggplot(obs) +
+    geom_bar(
+      aes(
+        x = date, y = sequences,
+        fill = clades_modeled
+      ),
+      stat = "identity",
+      position = "stack"
+    ) +
+    facet_wrap(~location, ncol = 3) +
+    get_plot_theme(dates = TRUE) +
+    scale_fill_manual(
+      name = "Clade",
+      values = plot_comps$clade_colors
+    ) +
+    xlab("") +
+    ylab("Sequence counts\nfor evaluation") +
+    guides(
+      fill = guide_legend(
         title.position = "left",
         title.hjust = 0.5,
         nrow = 2
@@ -79,5 +122,56 @@ get_plot_model_pred_obs <- function(model_pred_obs_df,
     geom_vline(aes(xintercept = ymd(nowcast_date)),
       linetype = "dashed"
     )
+  return(p)
+}
+
+get_plot_scores_over_time <- function(scores,
+                                      locs,
+                                      nowcast_date) {
+  scores_filtered <- scores |>
+    filter(
+      location %in% locs,
+      nowcast_date == !!nowcast_date,
+      !is.na(energy_score)
+    )
+  plot_comps <- plot_components()
+  p <- ggplot(scores_filtered) +
+    geom_point(aes(
+      x = target_date, y = energy_score,
+      color = model,
+      alpha = scored
+    )) +
+    geom_line(aes(
+      x = target_date, y = energy_score,
+      color = model,
+      alpha = scored
+    )) +
+    facet_wrap(~location, ncol = 3) +
+    get_plot_theme(dates = TRUE) +
+    xlab("") +
+    ylab("Energy score") +
+    guides(
+      color = guide_legend(
+        title.position = "left",
+        title.hjust = 0.5,
+        nrow = 3
+      )
+    ) +
+    scale_color_manual(
+      name = "Model",
+      values = plot_comps$model_colors
+    ) +
+    scale_x_date(
+      limits = c(
+        min(scores_filtered$target_date),
+        max(scores_filtered$target_date)
+      ),
+      date_breaks = "1 week",
+      date_labels = "%d %b %Y"
+    ) +
+    geom_vline(aes(xintercept = ymd(nowcast_date)),
+      linetype = "dashed"
+    )
+
   return(p)
 }
--- targets/fig_pred_plus_data_targets.R
+++ targets/fig_pred_plus_data_targets.R
@@ -6,5 +6,20 @@ fig_pred_plus_data_targets <- list(
       eval_seq = clean_variant_data_for_eval,
       clades_to_plot = clades_to_plot
     )
+  ),
+  tar_target(
+    name = plot_seq_count_underlay,
+    command = get_plot_seq_count(
+      eval_seq = clean_variant_data_for_eval,
+      temporal_granularity = "days"
+    )
+  ),
+  tar_target(
+    name = plot_scores_underlay,
+    command = get_plot_scores_over_time(
+      scores = su_scores,
+      nowcast_date = nowcast_date_for_vis,
+      locs = states_for_vis
+    )
   )
 )
