3a670078cb35c007b87784a46b7320daf393f635
Kaitlyn Johnson <kaitlyn.johnson@lshtm.ac.uk>
2025-11-26 12:32:03 +0000

    remove commented code

--- R/fig_pred_plus_data.R
+++ R/fig_pred_plus_data.R
@@ -18,7 +18,6 @@ get_plot_model_pred_obs <- function(model_pred_obs_df,
     )
   }
 
-  # weekly_obs_data <- daily_to_weekly(eval_seq)
   total_seq <- eval_seq |>
     group_by(date, location) |>
     summarise(n_seq = sum(sequences))
